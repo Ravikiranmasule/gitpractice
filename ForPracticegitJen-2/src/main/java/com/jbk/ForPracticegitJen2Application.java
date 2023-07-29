b4b2670a4b886b5562fbd7c03e33dbecd20e0595
Ravikiranmasule <ravikiran.masule@gmail.com>
2023-07-29 17:05:49 +0530

    first commit

---
+++ ForPracticegitJen-2/src/main/java/com/jbk/ForPracticegitJen2Application.java
@@ -0,0 +1,13 @@
+package com.jbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForPracticegitJen2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForPracticegitJen2Application.class, args);
+	}
+
+}
